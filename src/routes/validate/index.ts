dfebbecf3a1cc6cfe191f266bc915d5186066e1a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-01-28 21:18:41 +0100

    Added validator module

--- src/routes/validate/index.ts
+++ src/routes/validate/index.ts
@@ -1,8 +1,15 @@
 import { Router } from 'express'
+import { validateRuleField } from './validateRuleField'
+import { validateDataField } from './validateDataField'
 import { validateController } from './validate.controller'
 
 const validateRuleRoute = Router()
 
-validateRuleRoute.post('/', validateController)
+validateRuleRoute.post(
+  '/',
+  validateRuleField,
+  validateDataField,
+  validateController
+)
 
 export { validateRuleRoute }
